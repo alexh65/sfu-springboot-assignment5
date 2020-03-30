c6ae4727cf3dce17f0e3a9f8ab02923dee913da9
Riya Dhariwal <rdhariwa@sfu.ca>
2020-03-29 20:59:11 -0700

    Finished Semester

--- src/main/java/ca/coursePlanner/Test.java
+++ src/main/java/ca/coursePlanner/Test.java
@@ -7,11 +7,10 @@ import java.util.ArrayList;
 public class Test {
 
     public static void main(String[] args) {
-        CSVReader cvs = new CSVReader();
-        ArrayList<String> test = cvs.getCvsList();
-
-        for (String s : test) {
-            System.out.println(s);
-        }
+        String test = "1234";
+        String s1 = test.substring(0, 3);
+        String s2 = test.substring(3);
+        System.out.println(s1);
+        System.out.println(s2);
     }
 }
--- src/main/java/ca/coursePlanner/model/Semester.java
+++ src/main/java/ca/coursePlanner/model/Semester.java
@@ -1,4 +1,22 @@
 package ca.coursePlanner.model;
 
+/**
+ * Class stores information on a single semester.
+ */
 public class Semester {
+    private int year;
+    private int semester;
+
+    public Semester(String semester) {
+        this.year = Integer.parseInt(semester.substring(0, 3));
+        this.semester = Integer.parseInt(semester.substring(3));
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
 }
