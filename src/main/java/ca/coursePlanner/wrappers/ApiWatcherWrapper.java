a7d5ae80c12af81511412c04a6aa544f64598fe8
Riya Dhariwal <rdhariwa@sfu.ca>
2020-04-07 19:28:06 -0700

    Finished wrapper class.

--- src/main/java/ca/coursePlanner/wrappers/ApiWatcherWrapper.java
+++ src/main/java/ca/coursePlanner/wrappers/ApiWatcherWrapper.java
@@ -7,4 +7,15 @@ public class ApiWatcherWrapper {
     public ApiCourseWrapper course;
     public List<String> events;
 
+    public static ApiWatcherWrapper makeWatcherWrapper (long watcherId, ApiDepartmentWrapper department,
+                                                        ApiCourseWrapper course, List<String> events){
+        ApiWatcherWrapper result = new ApiWatcherWrapper();
+        result.id = watcherId;
+        result.department = department;
+        result.course = course;
+        result.events = events;
+
+        return result;
+    }
+
 }
